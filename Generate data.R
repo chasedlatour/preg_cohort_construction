b0b50144bfff1697ea9db75768ffe370e38886c0
chasedlatour <chasedlatour@gmail.com>
2024-06-21 13:34:56 -0400

    Modifying description code and the data generation distributions.

--- Generate data.R
+++ Generate data.R
@@ -43,7 +43,7 @@ generate_dgm <- function(param_file, save_name_gen,
   ## Create the parameter list that are the same across simulations
   params_list_gen <- list(
     n_sim = 1,
-    n = 10000, #5000,
+    n = 15000, #5000,
     p_sev_dist = c(1/3, 1/3, 1/3), # Even distribution
     p_trt_sev = c(0.35, 0.50, 0.65),
     p_indx_pnc = c(0.27, 0.335, 0.395),
