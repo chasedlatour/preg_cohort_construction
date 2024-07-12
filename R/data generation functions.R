a05b33f676bda5ba9afc35a90bf5b11666b53dce
chasedlatour <chasedlatour@gmail.com>
2024-07-12 11:51:15 -0400

    Finalized distribution of pregnancy cohort. Now, need to determine the distribution of prenatal care encounters.

--- R/data generation functions.R
+++ R/data generation functions.R
@@ -34,7 +34,7 @@ generate_dgm <- function(n_sim, n, param_file,
     n_sim = n_sim,
     n = n, #5000,
     p_sev_dist = c(1/3, 1/3, 1/3), # Even distribution
-    p_trt_sev = c(0.35, 0.50, 0.65),
+    p_trt_sev = c(0.4, 0.50, 0.6),
     p_indx_pnc = c(0.23, 0.33, 0.44), #c(0.27, 0.335, 0.395),
     potential_preg_trt = potential_preg_trt,
     potential_preg_untrt = potential_preg_untrt,
